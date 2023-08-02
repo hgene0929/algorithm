a0eaa55514bba0b185f14a7c66f5f9c0c425daa7
hgene <90823532+hgene0929@users.noreply.github.com>
2023-08-02 15:14:55 +0900

    [Silver III] Title: N과 M (1), Time: 316 ms, Memory: 30832 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int n;
+	static int m;
+	static int[] num;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// input
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		num = new int[m];
+		visited = new boolean[n+1];
+
+		// solution
+		permutation(0);
+
+		// output
+		bw.flush();
+		bw.close();
+	}
+
+	static void permutation(int cnt) throws Exception {
+		if(cnt == m) {
+			for(int i=0; i<m; i++) {
+				bw.write(num[i]+" ");
+			}
+			bw.write("\n");
+			return;
+		}
+		for(int i=1; i<=n; i++) {
+			if(visited[i]) {
+				continue;
+			}
+			num[cnt] = i;
+			visited[i] = true;
+			permutation(cnt+1);
+			visited[i] = false;
+		}
+	}
+}
