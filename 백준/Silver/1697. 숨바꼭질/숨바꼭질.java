9d1ce66368f9c699cdc18d9ca4f7a2bcab846273
hgene <90823532+hgene0929@users.noreply.github.com>
2023-08-21 14:13:45 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 17100 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,48 +2,55 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int minCnt = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		
 		// input
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-
+		
 		// solution
-		// 연결된 숫자를 돌면서 bfs
-		int min = Integer.MAX_VALUE;
-		Deque<int[]> deque = new ArrayDeque<>();
+		solution(N, K);
+		
+		// output
+		System.out.println(minCnt);
+	}
+
+	static void solution(int N, int K) {
+		Queue<int[]> queue = new ArrayDeque<>();
 		boolean[] visited = new boolean[100001];
-		deque.offerLast(new int[] {N, 0});
+		queue.offer(new int[] { N,0 });
 		visited[N] = true;
-
-		while(!deque.isEmpty()) {
-			int[] now = deque.pollFirst();
+		
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
 			if(now[0] == K) {
-				min = Math.min(min, now[1]);
+				minCnt = minCnt > now[1] ? now[1] : minCnt;
 			}
-
-			int next;
-			for(int i=0; i<3; i++) {
-				if(i == 0) {
-					next = now[0]+1;
-				} else if(i == 1) {
-					next = now[0]-1;
-				} else {
-					next = now[0]*2;
-				}
-				if(next >= 0 && next < 100001 && !visited[next]) {
-					deque.offerLast(new int[] {next, now[1]+1});
-					visited[next] = true;
-				}
+			
+			// X + 1
+			int next = now[0] + 1;
+			if(next >= 0 && next <= 100000 && !visited[next]) {
+				queue.offer(new int[] { next,now[1]+1 });
+				visited[next] = true;
+			}
+			
+			// X - 1
+			next = now[0] - 1;
+			if(next >= 0 && next <= 100000 && !visited[next]) {
+				queue.offer(new int[] { next,now[1]+1 });
+				visited[next] = true;
+			}
+			
+			// X * 2
+			next = now[0] * 2;
+			if(next >= 0 && next <= 100000 && !visited[next]) {
+				queue.offer(new int[] { next,now[1]+1 });
+				visited[next] = true;
 			}
 		}
-
-		// output
-		bw.write(min+"");
-		bw.flush();
-		bw.close();
 	}
 }
